ba94eebe972976123d9fd068cdbbb966e42f7661
Joules <joules.hdz@gmail.com>
2021-11-03 23:21:04 -0600

    d

--- jdh/resources/stock_data.py
+++ jdh/resources/stock_data.py
@@ -2,6 +2,7 @@
 from flask import request
 import yfinance as yf
 import redis
+import requests
 
 # Local
 from utils.symbols import symbols
@@ -14,10 +15,28 @@ import json
 
 r = redis.Redis.from_url(os.getenv('REDIS_URL'))
 
+url = "https://currency-converter5.p.rapidapi.com/currency/convert"
+querystring = {"format": "json", "from": "USD", "to": "MXN", "amount": "1"}
+headers = {
+    'x-rapidapi-host': "currency-converter5.p.rapidapi.com",
+    'x-rapidapi-key': os.getenv('CURRENCY_API_KEY')
+    }
+
+
+def currency():
+    if r.exists('MXN'):
+        return float(r.get('MXN'))
+    else:
+        response = requests.request("GET", url, headers=headers, params=querystring).json()['rates']['MXN']['rate']
+        r.set('MXN', response)
+        r.expire('MXN', 60 * 60 * 1)
+        return float(response)
+
+
 def get(symbol=None):
 
     if not symbol:
-        symbol=request.get_json()['symbol']
+        symbol = request.get_json()['symbol']
 
     if r.exists(symbol):
         print(f'Leyendo desde cache datos: {r.memory_usage(symbol)}', flush=True)
@@ -25,13 +44,18 @@ def get(symbol=None):
     else:
         print('Consultando datos', flush=True)
         data = yf.Ticker(symbol).history(period="D1", start=str(date(2021, 5, 1)))
-        close_values = data.Close.to_list()
-        labels=[str(date).replace(' 00:00:00', '') for date in data.index]
+        if '.MX' in symbol:
+            close_values = data.Close.to_list()
+        else:
+            # Convertir a pesos mexicanos
+            mxn = currency()
+            close_values = [d*mxn for d in data.Close.to_list()]
+        labels = [str(datee).replace(' 00:00:00', '') for datee in data.index]
         data = dict(data=close_values, labels=labels, symbol=symbol)
         r.set(symbol, json.dumps(data))
         r.expire(symbol, 60*60*12)
 
-    return data # {'symbol': symbol, 'data':close_values, 'labels':labels}
+    return data  # {'symbol': symbol, 'data':close_values, 'labels':labels}
 
 
 def restore(key):
